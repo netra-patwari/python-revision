595103db3a78d79e890dbc784237dce4967e3739
netra-patwari <patwarinetra@gmail.com>
2023-12-08 19:13:36 +0530

    first commit

---
+++ basics/basic_1.py
@@ -0,0 +1,110 @@
+# # Type casting
+# a = 5
+# a  = str(a)
+
+# print(type(a))
+
+
+## string methods
+
+# a = "Hello World"
+
+# print(len(a))
+
+
+# a = int(input("Enter a number: "))
+
+# if a >= 10:
+#     print("YO")
+    
+# else :
+#         print("COOL")
+
+
+# a = "Happy Birthday"
+# print(a[0:5])
+
+# a = "cool"
+
+# x = a.capitalize()
+# print(x)
+
+
+# x = a.casefold()
+# print(x)
+
+
+# x = a.center(10, ' ')
+# print(x)
+
+
+# Operarors
+# a= 5  
+# b= 4
+
+# print(a + b)
+# print(a - b )
+# print(a * b )
+# print(a / b )
+# print(a ** b )
+# print(a ^ b )
+
+# LIST
+# list = [1,2,3,"4",5,"6"]
+
+# True -->1 , False ----> 0
+# print( False + 5)
+
+
+# TUPLE
+# thistuple = ("apple", "banana", "cherry")
+# print(type(thistuple))
+# print(thistuple.index("cherry"))
+
+
+# list = [1,23,4]
+# mylist = list.copy()
+# list = list.append(5)
+# print(mylist)
+
+
+# s = {1, 2, 3, 3}
+# print(s)
+
+# a = {
+#     "password" : "admin" ,
+#     "name" : "netra",
+    
+# }
+
+# print(type(a))
+# print(a)
+# print(a["password"])
+# print(a["name"])
+
+# print(a.items())
+# print(a.keys())
+
+
+# a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+# else:
+#   print("a is greater than b")
+
+# i = 1
+# while i < 6:
+#   print( ' * ' * i)
+#   i += 1
+  
+# for x in range(7):
+#   print(x * " * ")
+
+
+# def add(a,b):
+#  print(a+b)
+ 
+# add(111,2)
